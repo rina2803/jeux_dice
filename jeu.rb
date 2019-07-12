2781210f1ccac833e0f48be69c530f471679d63b
rina2803 <buthnet92@gmail.com>
2019-07-12 16:47:09 +0300

    tsizy le teo

--- jeu.rb
+++ jeu.rb
@@ -1,4 +1,32 @@
-def hello
-	puts"bonjour"
+puts "Bienvenue dans le jeu d'ascensseur"
+print "Entrer le nombre d'etage que vous voulez jouer : "
+n = Integer(gets.chomp)
+pos = 0
+print "Appuiez sur j pour jouer : "
+a = gets.chomp
+while a == "j"
+    x = rand(1..6)
+    if pos != n
+        case x
+        when 5,6
+            pos += 1
+            puts " \n vous êtes dans le #{pos} étage \n "
+        when 1
+            if pos == 1
+                puts " \n vous êtes dans le #{pos} étage \n"
+            else
+                pos -= 1
+                puts " \n vous êtes dans le #{pos} étage \n "
+            end
+        else
+            puts " \n vous êtes dans le #{pos} étage \n "
+        end
+
+        a = gets.chomp
+    else
+        puts "**** Bravo, vous êtes arrivés ****"    
+        break;
+    
+    end
+    
 end
-hello
